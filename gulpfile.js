cad3bfa17d0d81c9a06143de11fc0b98b3b13ac6
Lupo Montero <lupomontero@gmail.com>
2016-03-23 18:57:54 -0500

    updates deps and replaces jshing with eslint hapi style

--- gulpfile.js
+++ gulpfile.js
@@ -1,89 +1,106 @@
-var fs = require('fs');
-var path = require('path');
-var gulp = require('gulp');
-var jshint = require('gulp-jshint');
-var minifyCss = require('gulp-minify-css');
-var uglify = require('gulp-uglify');
-var rename = require('gulp-rename');
-var Pageres = require('pageres');
-var rimraf = require('rimraf');
-var replace = require('gulp-replace');
-var zip = require('gulp-zip');
-var pkg = require('./package.json');
+'use strict';
 
 
-gulp.task('lint', function () {
-  return gulp.src([
-    'gulpfile.js', 'src/**/*.js', 'test/**/*.js'
-  ])
-    .pipe(jshint())
-    .pipe(jshint.reporter('jshint-stylish'));
+const Fs = require('fs');
+const Path = require('path');
+const Gulp = require('gulp');
+const Eslint = require('gulp-eslint');
+const MinifyCss = require('gulp-minify-css');
+const Uglify = require('gulp-uglify');
+const Rename = require('gulp-rename');
+const Pageres = require('pageres');
+const Rimraf = require('rimraf');
+const Replace = require('gulp-replace');
+const Zip = require('gulp-zip');
+const Pkg = require('./package.json');
+
+
+Gulp.task('lint', () => {
+
+  return Gulp.src(['**/*.js'])
+    .pipe(Eslint())
+    .pipe(Eslint.format())
+    .pipe(Eslint.failAfterError());
 });
 
 
-gulp.task('uglify', function () {
-  return gulp.src( [ './src/main.js'] )
-    .pipe(uglify())
-    .pipe(rename({ extname: '.min.js' }))
-    .pipe(gulp.dest('dist'));
+Gulp.task('uglify', () => {
+
+  return Gulp.src(['./src/main.js'])
+    .pipe(Uglify())
+    .pipe(Rename({ extname: '.min.js' }))
+    .pipe(Gulp.dest('dist'));
 });
 
 
-gulp.task('css', function () {
-  return gulp.src([ 'src/style.css' ])
-    .pipe(minifyCss())
-    .pipe(gulp.dest('dist'));
+Gulp.task('css', () => {
+
+  return Gulp.src(['src/style.css'])
+    .pipe(MinifyCss())
+    .pipe(Gulp.dest('dist'));
 });
 
 
-gulp.task('screenshot', function (done) {
-  var host = '192.168.99.100';
-  var port = 8080;
-  var dest = path.join(__dirname, 'tmp');
-  var pageres = new Pageres({ delay: 2 })
-    .src(host + ':' + port, ['880x660' ]/*, { crop: true }*/)
+Gulp.task('screenshot', (done) => {
+
+  const host = '192.168.99.100';
+  const port = 8080;
+  const dest = Path.join(__dirname, 'tmp');
+  const pageres = new Pageres({ delay: 2 })
+    .src(host + ':' + port, ['880x660']/*, { crop: true }*/)
     .dest(dest);
 
-  pageres.run(function (err) {
-    if (err) { return done(err); }
-    fs.renameSync(
-      path.join(dest, host + '!' + port + '-880x660.png'),
-      path.join(__dirname, 'dist', 'screenshot.png')
+  pageres.run((err) => {
+
+    if (err) {
+      return done(err);
+    }
+
+    Fs.renameSync(
+      Path.join(dest, host + '!' + port + '-880x660.png'),
+      Path.join(__dirname, 'dist', 'screenshot.png')
     );
-    rimraf(dest, done);
+
+    Rimraf(dest, done);
   });
 });
 
 
-gulp.task('copy:src', function () {
-  return gulp.src([ '**/*.php', 'main.js' ], { cwd: 'src' })
-    .pipe(replace(
-      "'main', get_template_directory_uri() . '/main.js'",
-      "'main', get_template_directory_uri() . '/main.min.js'"
+Gulp.task('copy:src', () => {
+
+  return Gulp.src(['**/*.php', 'main.js'], { cwd: 'src' })
+    .pipe(Replace(
+      '\'main\', get_template_directory_uri() . \'/main.js\'',
+      '\'main\', get_template_directory_uri() . \'/main.min.js\''
     ))
-    .pipe(gulp.dest('dist'));
-});
-
-gulp.task('copy:lang', function () {
-  return gulp.src([ '*' ], { cwd: 'src/languages' })
-    .pipe(gulp.dest('dist/languages'));
-});
-
-gulp.task('copy', [ 'copy:src', 'copy:lang' ]);
-
-
-gulp.task('zip', [ 'build' ], function () {
-  return gulp.src('dist/**/*')
-    .pipe(zip(pkg.name + '-' + pkg.version + '.zip'))
-    .pipe(gulp.dest('.'));
+    .pipe(Gulp.dest('dist'));
 });
 
 
-gulp.task('watch', function () {
-  gulp.watch([ 'src/**/*' ], [ 'build' ]);
+Gulp.task('copy:lang', () => {
+
+  return Gulp.src(['*'], { cwd: 'src/languages' })
+    .pipe(Gulp.dest('dist/languages'));
 });
 
 
-gulp.task('build', [ 'uglify', 'css', 'copy' ]);
-gulp.task('default', [ 'lint', 'build' ]);
+Gulp.task('copy', ['copy:src', 'copy:lang']);
+
+
+Gulp.task('zip', ['build'], () => {
+
+  return Gulp.src('dist/**/*')
+    .pipe(Zip(Pkg.name + '-' + Pkg.version + '.zip'))
+    .pipe(Gulp.dest('.'));
+});
+
+
+Gulp.task('watch', () => {
+
+  Gulp.watch(['src/**/*'], ['build']);
+});
+
+
+Gulp.task('build', ['uglify', 'css', 'copy']);
+Gulp.task('default', ['lint', 'build']);
 
--- src/main.js
+++ src/main.js
@@ -1,3 +1,7 @@
+/*eslint no-var:0, prefer-arrow-callback: 0 */
+'use strict';
+
+
 jQuery(function ($) {
 
   $('html').removeClass('no-js');
